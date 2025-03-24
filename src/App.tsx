b1d8add72c3f8ed57d3d1a39b76707c10afa0d5f
Allen Haws <allen.haws@ctr.govcio.com>
2025-03-24 15:35:31 -0700

    Added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 const client = generateClient<Schema>();
 
-function App() {
+function App({ signOut }: { signOut: () => void }) {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -19,22 +21,20 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "1rem" }}>
+        <h1>My todos</h1>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
     </main>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  hideSignUp: false,
+});
